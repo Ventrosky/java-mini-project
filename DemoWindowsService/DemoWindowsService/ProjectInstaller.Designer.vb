1e79e9b11ccfa930f16a843f52543e15bf1043c4
Salvatore Ventrone <ventrosky@gmail.com>
2019-04-08 23:04:36 +0200

    Windows Service
    Windows Service Demo in VB Net

---
+++ DemoWindowsService/DemoWindowsService/ProjectInstaller.Designer.vb
@@ -0,0 +1,45 @@
+﻿<System.ComponentModel.RunInstaller(True)> Partial Class ProjectInstaller
+    Inherits System.Configuration.Install.Installer
+
+    'Installer esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione componenti
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione componenti
+    'Può essere modificata  in Progettazione componenti.
+    'Non modificarla mediante l'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ServiceProcessInstaller1 = New System.ServiceProcess.ServiceProcessInstaller()
+        Me.DemoWinService = New System.ServiceProcess.ServiceInstaller()
+        '
+        'ServiceProcessInstaller1
+        '
+        Me.ServiceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem
+        Me.ServiceProcessInstaller1.Password = Nothing
+        Me.ServiceProcessInstaller1.Username = Nothing
+        '
+        'DemoWinService
+        '
+        Me.DemoWinService.ServiceName = "Windows Service Demo"
+        '
+        'ProjectInstaller
+        '
+        Me.Installers.AddRange(New System.Configuration.Install.Installer() {Me.ServiceProcessInstaller1, Me.DemoWinService})
+
+    End Sub
+
+    Friend WithEvents ServiceProcessInstaller1 As ServiceProcess.ServiceProcessInstaller
+    Friend WithEvents DemoWinService As ServiceProcess.ServiceInstaller
+End Class
---
+++ DemoWindowsService/DemoWindowsService/ServiceDemo.vb
@@ -0,0 +1,51 @@
+﻿Public Class ServiceDemo
+
+    Dim tmr As Timers.Timer
+    Const msec As Integer = 1000 ' 1 sec
+    Const PATH_IN As String = "C:\Temp\input\"
+    Const PATH_OUT As String = "C:\Temp\output\"
+
+    Protected Overrides Sub OnStart(ByVal args() As String)
+        ' avviare il proprio servizio. 
+        tmr = New Timers.Timer
+        tmr.Interval = msec
+        AddHandler tmr.Elapsed, AddressOf TickHandler
+        tmr.Enabled = True
+
+        If (Not IO.Directory.Exists(PATH_IN)) Then
+            IO.Directory.CreateDirectory(PATH_IN)
+        End If
+        If (Not IO.Directory.Exists(PATH_OUT)) Then
+            IO.Directory.CreateDirectory(PATH_OUT)
+        End If
+
+        FileIO.WriteToFile($"[*] Service Started {vbNewLine}")
+    End Sub
+
+    Protected Overrides Sub OnStop()
+        ' arrestare il proprio servizio.
+        tmr.Enabled = False
+        FileIO.WriteToFile($"[*] Service Stopped {vbNewLine}")
+    End Sub
+
+    Private Sub TickHandler(obj As Object, e As EventArgs)
+        FileIO.WriteToFile($"[*] Tick {Math.Floor(msec / 1000)} Secs {vbNewLine}")
+    End Sub
+
+    Private Sub PollingFolder()
+        Dim files() As String = IO.Directory.GetFiles(PATH_IN)
+        For Each fileName As String In files
+            Dim text As String = IO.File.ReadAllText(fileName)
+
+            FileIO.WriteToFile($"[*] Found: {fileName} {vbNewLine}")
+
+            Dim time As DateTime = DateTime.Now
+            Dim format As String = "ddMMHHmmyyyy"
+            Dim tempName As String = $"{time.ToString(format)}.txt"
+
+            IO.File.Move(fileName, $"{PATH_OUT}{tempName}")
+            FileIO.WriteToFile($"[*] Moved: {tempName} {vbNewLine}")
+        Next
+    End Sub
+
+End Class
