96d7545a74bac04b1d183e70431bca2eaaadbf6d
Salvatore Ventrone <ventrosky@gmail.com>
2019-10-05 22:38:04 +0200

    Spring Boot Essentials
    Udemy course "Spring Boot For Beginners"

---
+++ spring-boot-essentials/src/main/java/com/ventrosky/demo/dao/FakeStudentDaoImpl.java
@@ -0,0 +1,45 @@
+package com.ventrosky.demo.dao;
+
+import com.ventrosky.demo.model.Student;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository("fakeDao")
+public class FakeStudentDaoImpl implements StudentDao {
+    private final Map<UUID, Student> database;
+
+    public FakeStudentDaoImpl() {
+        this.database = new HashMap<>();
+        UUID studentId = UUID.randomUUID();
+        database.put(studentId, new Student(studentId, 24, "Sal", "Ven", "CS"));
+    }
+
+    @Override
+    public int insertNewStudent(UUID studentId, Student student) {
+        database.put(studentId, student);
+        return 0;
+    }
+
+    @Override
+    public Student selectStudentById(UUID studentId) {
+        return database.get(studentId);
+    }
+
+    @Override
+    public List<Student> selectAllStudents() {
+        return new ArrayList<>(database.values());
+    }
+
+    @Override
+    public int updateStudentById(UUID studentId, Student studentUpdate) {
+        database.put(studentId, studentUpdate);
+        return 1;
+    }
+
+    @Override
+    public int deleteStudentById(UUID studentId) {
+        database.remove(studentId);
+        return 1;
+    }
+}
